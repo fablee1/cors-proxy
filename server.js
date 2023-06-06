328c729c951636913c29fb6673bf272a965d2dbb
FaBLee <58670304+fablee1@users.noreply.github.com>
2023-06-06 17:24:41 +0300

    test3

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const axios = require("axios")
+const cors = require("cors")
 const app = express()
 const port = 3000
+
+app.use(cors())
 
 app.post("/query", async (req, res) => {
   const { data } = await axios.post("https://graphigo.prd.space.id/query", req.body)
