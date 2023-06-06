ae3eaa2e58acb57cac76d6020ff18fbc2720e321
FaBLee <58670304+fablee1@users.noreply.github.com>
2023-06-06 17:19:41 +0300

    test

--- server.js
+++ server.js
@@ -8,7 +8,39 @@ console.log("Using limit: ", myLimit)
 
 app.use(bodyParser.json({ limit: myLimit }))
 
-app.all("*", function (req, res, next) {
+// app.all("*", function (req, res, next) {
+//   // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
+//   res.header("Access-Control-Allow-Origin", "*")
+//   res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE")
+//   res.header("Access-Control-Allow-Headers", req.header("access-control-request-headers"))
+
+//   if (req.method === "OPTIONS") {
+//     // CORS Preflight
+//     res.send()
+//   } else {
+//     var targetURL = req.header("Target-URL")
+//     if (!targetURL) {
+//       res.send(500, { error: "There is no Target-Endpoint header in the request" })
+//       return
+//     }
+//     request(
+//       {
+//         url: targetURL + req.url,
+//         method: req.method,
+//         json: req.body,
+//         headers: { Authorization: req.header("Authorization") },
+//       },
+//       function (error, response, body) {
+//         if (error) {
+//           console.error("error: " + response.statusCode)
+//         }
+//         //                console.log(body);
+//       }
+//     ).pipe(res)
+//   }
+// })
+
+app.post("/query", function (req, res, next) {
   // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE")
@@ -18,17 +50,11 @@ app.all("*", function (req, res, next) {
     // CORS Preflight
     res.send()
   } else {
-    var targetURL = req.header("Target-URL")
-    if (!targetURL) {
-      res.send(500, { error: "There is no Target-Endpoint header in the request" })
-      return
-    }
     request(
       {
-        url: targetURL + req.url,
-        method: req.method,
+        url: "https://graphigo.prd.space.id/query",
+        method: "POST",
         json: req.body,
-        headers: { Authorization: req.header("Authorization") },
       },
       function (error, response, body) {
         if (error) {
